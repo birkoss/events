97e68b8b53ba58fc01ab27220aedfa21f47494cc
birkoss <admin@birkoss.com>
2016-10-10 16:15:01 -0400

    Initial commit

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class EventController extends Controller {
+
+	public function show() {
+		$events = [
+			'aaa'=>'abc',
+			'bbb'=>'def'
+		];
+
+		return view('welcome')->withEvents($events);
+	}
+
+}
