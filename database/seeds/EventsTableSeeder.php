9c38cea2cd9d125779f97307ffbe6a2447546bc7
= <admin@birkoss.com>
2016-10-11 13:26:57 -0400

    Adding Category Event links in Seeder

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -7,6 +7,7 @@ use Faker\Factory as Faker;
 class EventsTableSeeder extends Seeder {
 	public function run() {
 		DB::table('events')->truncate();
+		DB::table('category_event')->truncate();
 
 		$faker = Faker::create();
 		for($i=1; $i<=15; $i++) {
@@ -14,7 +15,17 @@ class EventsTableSeeder extends Seeder {
 			$event->name = $faker->sentence(6, true);
 			$event->content = $faker->text(400);
 			$event->visible_at = $faker->date($format = 'Y-m-d', $max = 'now');
+
 			$event->save();
+
+			for($j=1; $j<mt_rand(1, 3); $j++) {
+				$category = App\Category::find($j);
+				$event->categories()->save($category);
+//				$category->events()->save($event);
+
+				echo 'Event:'.$event->id.', Category:'.$category->id.chr(10);
+			}
+
 		}
 	}
 }
